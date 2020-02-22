298a14481d02b8764d7ee5e45f0c2198c708ab05
Riccosblogger <riccosblogger@gmail.com>
2020-02-22 16:58:04 +0300

    feat: add the solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0) return 0;
+  var min_arr = array[0];
+  for (let i=1; i<array.length; i++) if (array[i]<min_arr) min_arr = array[i];
+  return min_arr; 
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0) return 0;
+  var max_arr = array[0];
+  for (let i=1; i<array.length; i++) if (array[i]>max_arr) max_arr = array[i];
+  return max_arr; 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length == 0) return 0;
+  var result = ( array.reduce((sum, current) => sum + current)) / array.length ;
+    return result;
 }
